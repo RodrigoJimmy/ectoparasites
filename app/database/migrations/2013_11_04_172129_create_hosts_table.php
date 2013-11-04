c60dea09a2e49c71e71fab4c85521ea81df4e5a5
Rodrigo de Lima Vieira <rodrigodelimavieira@gmail.com>
2013-11-04 15:56:46 -0200

    create_hosts_table migration
    Migrate para criar/excluir tabela hosts

---
+++ app/database/migrations/2013_11_04_172129_create_hosts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateHostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('hosts', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('species');
+			$table->string('family');
+			$table->timestamps();
+		});	
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('hosts');
+	}
+
+}
