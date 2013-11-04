a5b0cf227cf7e8af9c6353275554e454692d77e9
Rodrigo de Lima Vieira <rodrigodelimavieira@gmail.com>
2013-11-04 15:51:45 -0200

    create_countries_table migrate
    Migrate para criar/excluir tabela countries

---
+++ app/database/migrations/2013_11_04_155552_create_countries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCountriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('countries', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name')->unique();
+			$table->integer('continent_id')->unsigned();
+			$table->timestamps();
+			
+			$table->foreign('continent_id')->references('id')->on('continents');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('countries');
+	}
+
+}
