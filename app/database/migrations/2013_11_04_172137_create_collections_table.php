b5dbe13c439441479404fa5579303362523e4eea
Rodrigo de Lima Vieira <rodrigodelimavieira@gmail.com>
2013-11-04 15:57:11 -0200

    create_collections_table migration
    Migrate para criar/excluir tabela collections

---
+++ app/database/migrations/2013_11_04_172137_create_collections_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCollectionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('collections', function(Blueprint $table)
+		{
+			$table->increments('id');
+
+			$table->integer('species_id')->unsigned();
+			$table->integer('host_id')->unsigned();
+			$table->integer('collector_id')->unsigned();
+			$table->date('collection_start_date');
+			$table->date('collection_end_date');
+			$table->string('lot_number');
+			$table->string('site_number');
+			$table->integer('elevation');
+			$table->timestamps();
+
+			$table->foreign('species_id')->references('id')->on('species');
+			$table->foreign('host_id')->references('id')->on('hosts');
+			$table->foreign('collector_id')->references('id')->on('collectors');
+
+		});	
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('collections');
+	}
+
+}
